5253f354c9b454b5f5cf5901f0c6f465423483d3
JAlvarezJarreta <jalvarez@ebi.ac.uk>
2024-03-24 20:38:48 +0000

    run black and code clean-up

--- MEvoLibGUI/MEvoLibGUI/urls.py
+++ MEvoLibGUI/MEvoLibGUI/urls.py
@@ -12,9 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""
-URL configuration for MEvoLibGUI project.
+"""URL configuration for MEvoLibGUI project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -29,10 +27,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('nextflowApp.urls'))
-]
+
+urlpatterns = [path("admin/", admin.site.urls), path("", include("nextflowApp.urls"))]
--- MEvoLibGUI/nextflowApp/models.py
+++ MEvoLibGUI/nextflowApp/models.py
@@ -13,15 +13,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.db import models
 from pathlib import Path
+
+from django.db import models
 
 from MEvoLibGUI.settings import NEXTFLOW_UPLOADS_ROOT as ur
 
 
 # Create your models here.
 
-class FullWorkflowDocument(models.Model):   # Model to store a full workflow file.
-    save_path = Path(ur).joinpath("full_workflow")
-    docfile = models.FileField(upload_to=save_path)    
 
+class FullWorkflowDocument(models.Model):
+    """Model to store a full workflow file."""
+
+    save_path = Path(ur).joinpath("full_workflow")
+    docfile = models.FileField(upload_to=save_path)
